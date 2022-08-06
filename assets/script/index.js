998b9910f30345343b6d1f25233b7de68e0266b5
MageLink <mage.link.zhezard@gmail.com>
2022-08-06 19:37:23 -0400

    Fetch working

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,16 @@
-console.log("Ola k ase");
+let allData = undefined;
+
+const callApi = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    alert("Ha ocurrido un probleam con el servidor");
+    return;
+  }
+};
+
+window.addEventListener("DOMContentLoaded", async () => {
+  allData = await callApi("https://mindicador.cl/api/");
+});
