74e73df870bcec8f5e5c3c95930fbc337f70a944
MageLink <mage.link.zhezard@gmail.com>
2022-08-07 01:40:01 -0400

    All systems online

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,27 +1,61 @@
+const apiIp = "https://mindicador.cl/api/";
 let allData = undefined;
+let specificData = undefined;
 let moneyValue = 0;
+let myChart = "";
 
-const callApi = async (url) => {
+const callApi = async (url, tipo) => {
   try {
-    const res = await fetch(url);
+    const res = await fetch(`${url}${!tipo ? "" : tipo.toLowerCase()}`);
     const data = await res.json();
     return data;
   } catch (e) {
     document.querySelector("#total-on-clp").innerHTML =
-      "Ha ocurrido un probleam con el servidor. Por favor presione F5";
+      "Ha ocurrido un problema con el servidor. Por favor presione F5";
     return;
   }
+};
+
+const handleGraphic = (dataGraphic, selectedCurrency) => {
+  if (!dataGraphic.serie) {
+    return;
+  }
+  let graphicData = dataGraphic.serie.filter((elements, i) => {
+    if (i < 10) {
+      return elements;
+    }
+  });
+
+  const labels = graphicData.map((e) => e.fecha.substring(0, 10)).reverse();
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: selectedCurrency,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: graphicData.map((e) => e.valor),
+      },
+    ],
+  };
+
+  const config = {
+    type: "line",
+    data: data,
+    options: {},
+  };
+  myChart = new Chart(document.getElementById("myChart"), config);
 };
 
 const selectedCurrency = (option) => {
   moneyValue = 0;
   document.querySelector("#total-on-clp").innerHTML = "";
+  //document.querySelector("#myChart").innerHTML = "";
   if (option === "Euro" || option == "Bitcoin") {
     const filterValue = Object.values(allData).filter((e) => {
       return e.nombre === option;
     });
     moneyValue = filterValue[0].valor;
-    return;
   }
 };
 
@@ -29,7 +63,7 @@ const makeOperation = (value) => {
   return value / moneyValue;
 };
 
-const resultsOnCLP = () => {
+const resultsOnCLP = async () => {
   if (moneyValue === 0) {
     alert("Debe seleccionar una moneda");
     return;
@@ -40,13 +74,19 @@ const resultsOnCLP = () => {
     return;
   }
   document.querySelector("#total-on-clp").innerHTML = `$ ${makeOperation(valueOnInput)}`;
+
+  specificData = await callApi(apiIp, document.querySelector("#input-currency").value);
+  handleGraphic(specificData, document.querySelector("#input-currency").value);
 };
 
 window.addEventListener("DOMContentLoaded", async () => {
-  allData = await callApi("https://mindicador.cl/api/");
+  allData = await callApi(apiIp);
 });
 
 window.addEventListener("change", () => {
+  if (!isNaN(myChart.id)) {
+    myChart.destroy();
+  }
   const selectedValue = document.querySelector("#input-currency").value;
   selectedCurrency(selectedValue);
 });
