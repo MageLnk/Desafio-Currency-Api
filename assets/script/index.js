616aed66f6f797d17e88683398651ea92b25e493
MageLink <mage.link.zhezard@gmail.com>
2022-08-06 21:10:09 -0400

    Selected option working

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,11 +6,16 @@ const callApi = async (url) => {
     const data = await res.json();
     return data;
   } catch (e) {
-    alert("Ha ocurrido un probleam con el servidor");
+    alert("Ha ocurrido un probleam con el servidor. Por favor presione F5");
     return;
   }
 };
 
 window.addEventListener("DOMContentLoaded", async () => {
   allData = await callApi("https://mindicador.cl/api/");
+});
+
+window.addEventListener("change", () => {
+  const selectedValue = document.querySelector("#input-currency").value;
+  console.log(selectedValue);
 });
