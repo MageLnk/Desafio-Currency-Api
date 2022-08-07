32fa0711147d5a1a3d3f497a895db4544497639b
MageLink <mage.link.zhezard@gmail.com>
2022-08-06 21:53:36 -0400

    Make the math on the currency by CLP

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,4 +1,5 @@
 let allData = undefined;
+let moneyValue = 0;
 
 const callApi = async (url) => {
   try {
@@ -6,9 +7,39 @@ const callApi = async (url) => {
     const data = await res.json();
     return data;
   } catch (e) {
-    alert("Ha ocurrido un probleam con el servidor. Por favor presione F5");
+    document.querySelector("#total-on-clp").innerHTML =
+      "Ha ocurrido un probleam con el servidor. Por favor presione F5";
     return;
   }
+};
+
+const selectedCurrency = (option) => {
+  moneyValue = 0;
+  document.querySelector("#total-on-clp").innerHTML = "";
+  if (option === "Euro" || option == "Bitcoin") {
+    const filterValue = Object.values(allData).filter((e) => {
+      return e.nombre === option;
+    });
+    moneyValue = filterValue[0].valor;
+    return;
+  }
+};
+
+const makeOperation = (value) => {
+  return value / moneyValue;
+};
+
+const resultsOnCLP = () => {
+  if (moneyValue === 0) {
+    alert("Debe seleccionar una moneda");
+    return;
+  }
+  const valueOnInput = document.querySelector("#clp-value").value;
+  if (isNaN(valueOnInput)) {
+    alert("Debe ingresar solo números por favor");
+    return;
+  }
+  document.querySelector("#total-on-clp").innerHTML = `$ ${makeOperation(valueOnInput)}`;
 };
 
 window.addEventListener("DOMContentLoaded", async () => {
@@ -17,5 +48,5 @@ window.addEventListener("DOMContentLoaded", async () => {
 
 window.addEventListener("change", () => {
   const selectedValue = document.querySelector("#input-currency").value;
-  console.log(selectedValue);
+  selectedCurrency(selectedValue);
 });
